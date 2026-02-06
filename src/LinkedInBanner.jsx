1fba2c1deffd882b03a9d24b75b0c1110e94d922
konstantin-lubinskiy <kostya.lub14@gmail.com>
2026-02-06 16:39:30 +0300

    linkedin-banner

---
+++ src/LinkedInBanner.jsx
@@ -0,0 +1,195 @@
+import './LinkedInBanner.css'
+
+function LinkedInBanner() {
+  return (
+    <section className="linkedin-banner">
+      <div className="banner-bg">
+        <div className="banner-blob banner-blob-1"></div>
+        <div className="banner-blob banner-blob-2"></div>
+      </div>
+
+      <div className="banner-code-grid">
+        <div className="code-editor editor-1">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">const</span> solution = <span className="string">"success"</span>;</div>
+            <div className="code-line"><span className="keyword">return</span> &lt;Component /&gt;;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-2">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">async</span> function deploy() &#123;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;<span className="keyword">await</span> build();</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;<span className="keyword">return</span> <span className="string">"live"</span>;</div>
+            <div className="code-line">&#125;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-3">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line">&lt;<span className="keyword">App</span>&gt;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;&lt;<span className="keyword">Header</span> /&gt;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;&lt;<span className="keyword">Content</span> /&gt;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-4">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">npm</span> run deploy</div>
+            <div className="code-line"><span className="string">✓ Build success</span></div>
+            <div className="code-line"><span className="string">✓ Live now</span></div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-5">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">const</span>&nbsp;webCan&nbsp;=&nbsp;&#123;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;quality:&nbsp;<span className="string">"premium"</span>,</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;speed:&nbsp;<span className="string">"lightning"</span></div>
+            <div className="code-line">&#125;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-6">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">const</span>&nbsp;result&nbsp;=&nbsp;&#123;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;api:&nbsp;<span className="string">"ready"</span></div>
+            <div className="code-line">&#125;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-7">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">export</span>&nbsp;default App</div>
+            <div className="code-line"><span className="string">✓&nbsp;Connected</span></div>
+            <div className="code-line"><span className="string">✓&nbsp;Ready</span></div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-8">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">const</span>&nbsp;performance&nbsp;=&nbsp;&#123;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;score:&nbsp;<span className="string">"100%"</span></div>
+            <div className="code-line">&#125;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-9">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">import</span>&nbsp;&#123;&nbsp;create&nbsp;&#125;</div>
+            <div className="code-line">create(<span className="string">"success"</span>);</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-10">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">class</span>&nbsp;Handler&nbsp;&#123;</div>
+            <div className="code-line">&nbsp;&nbsp;&nbsp;init();&nbsp;</div>
+            <div className="code-line">&#125;</div>
+          </div>
+        </div>
+
+        <div className="code-editor editor-11">
+          <div className="editor-header">
+            <span className="dot red"></span>
+            <span className="dot yellow"></span>
+            <span className="dot green"></span>
+          </div>
+          <div className="editor-body">
+            <div className="code-line"><span className="keyword">const</span>&nbsp;api&nbsp;=&nbsp;create();</div>
+            <div className="code-line">api.connect();</div>
+            <div className="code-line">api.ready();</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="banner-content">
+        <div className="banner-text">
+          <h1 className="banner-name">
+            <span className="gradient-text">KANSTANTSIN LIUBINSKI</span>
+          </h1>
+          <p className="banner-role">Fullstack Developer</p>
+        </div>
+      </div>
+
+      <div className="tech-stack">
+        <span className="tech-tag">HTML</span>
+        <span className="tech-tag">CSS</span>
+        <span className="tech-tag">JavaScript</span>
+        <span className="tech-tag">TypeScript</span>
+        <span className="tech-tag">React</span>
+        <span className="tech-tag">React Native</span>
+        <span className="tech-tag">Redux Toolkit</span>
+        <span className="tech-tag">Zustand</span>
+        <span className="tech-tag">Node.js</span>
+        <span className="tech-tag">Webpack</span>
+        <span className="tech-tag">Vite</span>
+        <span className="tech-tag">Next.js</span>
+        <span className="tech-tag">SEO</span>
+        <span className="tech-tag">SSR</span>
+        <span className="tech-tag">SSG</span>
+        <span className="tech-tag">Express.js</span>
+        <span className="tech-tag">PostgreSQL</span>
+        <span className="tech-tag">React Testing Library</span>
+        <span className="tech-tag">Jest</span>
+        <span className="tech-tag">Vitest</span>
+        <span className="tech-tag">Playwright</span>
+        <span className="tech-tag">GitHub Actions</span>
+        <span className="tech-tag">Vercel</span>
+        <span className="tech-tag">Digital Ocean</span>
+      </div>
+    </section>
+  )
+}
+
+export default LinkedInBanner
